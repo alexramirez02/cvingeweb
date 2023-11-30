d027e7d31bb204cf003d1d1f8e73e79b961deb2b
alexramirez02 <alexanderd.ramirez@udea.edu.co>
2023-11-29 23:07:07 -0500

    primer commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,136 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { MdAirplay , MdMenuBook } from "react-icons/md";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+const Home = () => {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className="flex h-screen gap-1  bg-slate-200 debug">
+      <aside className="debug w-[305px] flex flex-col h-full bg-white  ">
+        <section className="debug p-1">
+          <div className="nombre1">
+            <h1>Andy Ramirez</h1>
+            <span> Desarrollador Backend</span>
+          </div>
+        </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <section className="debug">
+          <div className="flex flex-col px-1 py-1 ">
+            <div className=" todos ">
+              <span>Edad</span>
+              <span>42</span>
+            </div>
+            <div className="todos">
+              <span>Email</span>
+              <span>Alex</span>
+            </div>
+            <div className="todos">
+              <span>Tiempo Libre</span>
+              <span>Futbol</span>
+            </div>
+            <div className="todos">
+              <span>Linkeded </span>
+              <span>Link</span>
+            </div>
+          </div>
+        </section>
+        <section className="debug flex-col">
+          <h3>Idiomas</h3>
+          <div>
+            <div className="todos">
+              <span>Español</span>
+              <span>100%</span>
+            </div>
+            <div className="w-74 h-2 bg-yellow-500 debug"></div>
+          </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div>
+            <div className="todos">
+              <span>Ingles</span>
+              <span>20%</span>
+            </div>
+            <div className="w-20 h-2 bg-yellow-500 debug"></div>
+          </div>
+        </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+        <section className="debug flex-col">
+          <h3>Lenguajes De Programacion</h3>
+          <div>
+            <div className="todos">
+              <span>Html</span>
+              <span>60%</span>
+            </div>
+            <div className="w-40 h-2 bg-yellow-500 debug"></div>
+          </div>
+          <div>
+            <div className="todos">
+              <span>CSS</span>
+              <span>60%</span>
+            </div>
+            <div className="w-40 h-2 bg-yellow-500 debug"></div>
+          </div>
+          <div>
+            <div className="todos">
+              <span>Java</span>
+              <span>85%</span>
+            </div>
+            <div className="w-60 h-2 bg-yellow-500 debug"></div>
+          </div>
+          <div>
+            <div className="todos">
+              <span>Sprint</span>
+              <span>50%</span>
+            </div>
+            <div className="w-36 h-2 bg-yellow-500 debug"></div>
+          </div>
+          <div>
+            <div className="todos">
+              <span>Js</span>
+              <span>50%</span>
+            </div>
+            <div className="w-36 h-2 bg-yellow-500 debug"></div>
+          </div>
+        </section>
+        <section className="debug">
+          <h3>Complementos</h3>
+          <div className="debug todos">
+            <MdMenuBook />
+            <span>Lectura</span>
+          </div>
+          <div className="debug todos">
+            <MdAirplay />
+            <span>Television</span>
+          </div>
+        </section>
+      </aside>
+      <main className="debug  w-[988px] h-full  ">
+        <section className="h-5/6">
+          <section className="debug todos2 flex  ">
+            <div className="flex-col w-5/6">
+              <div className="debug h-3/4 flex flex-col items-center ">
+                <span>Andy Ramirez</span>
+                <p>
+                  Futuro Ingeniero en sistemas de la Universidad De Antioquia
+                  con anhelo de superación constante
+                </p>
+                
+              </div>
+              <div>Boton</div>
+            </div>
+            <div className="debug w-1/4 h-full bg-white">Foto</div>
+          </section>
+          <section className="debug  ">
+            <h3>Titulo</h3>
+            <p></p>
+            <div>
+              <div>Titulo1</div>
+              <div>Titulo2</div>
+              <div>Titulo3</div>
+            </div>
+          </section>
+          <section className="debug todos2 "></section>
+          <section className="debug todos2 "></section>
+        </section>
+      </main>
+      <footer className="debug  w-24  "></footer>
+    </div>
+  );
+};
+export default Home;
